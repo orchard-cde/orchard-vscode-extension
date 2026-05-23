61e1069c12057d3601f0ced68915bf031f6cb49d
Steven Tompkins <steven.tompkins.jr@gmail.com>
2026-05-22 22:50:51 -0500

    Add webpack config for webview bundle
    Install html-webpack-plugin, style-loader, and css-loader. Split
    webpack config into two targets: extension (Node/CommonJS) and
    webview (web/HTML with HtmlWebpackPlugin for the React SPA).

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@
 'use strict';
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
-const config = {
+const extensionConfig = {
   entry: './src/extension.ts',
   output: {
     filename: 'extension.js',
@@ -16,12 +17,12 @@ const config = {
     vscode: 'commonjs vscode',
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         exclude: /node_modules/,
         use: [{ loader: 'ts-loader' }],
       },
@@ -30,4 +31,38 @@ const config = {
   devtool: 'nosources-source-map',
 };
 
-module.exports = config;
+/** @type {import('webpack').Configuration} */
+const webviewConfig = {
+  entry: './src/webview/index.tsx',
+  output: {
+    filename: 'webview.js',
+    path: path.resolve(__dirname, 'dist', 'webview'),
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'ts-loader' }],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: false,
+      filename: 'index.html',
+      title: 'Orchard',
+    }),
+  ],
+  devtool: 'nosources-source-map',
+};
+
+module.exports = [extensionConfig, webviewConfig];
